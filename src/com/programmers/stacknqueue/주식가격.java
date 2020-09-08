523f64ed426ee05fe46504d7aa833fd1adcf5dd3
HyunJeong <dev.jhjhj@gmail.com>
2020-09-09 00:33:06 +0900

    programmers 스택&큐 주식가격

---
+++ src/com/programmers/stacknqueue/주식가격.java
@@ -0,0 +1,33 @@
+package com.programmers.stacknqueue;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] prices) {
+
+        int size = prices.length;
+        int[] answer = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (prices[i] > prices[j]) {
+                    answer[i] = j - i;
+                    break;
+                }
+                if (j == answer.length - 1) {
+                    answer[i] = j - i;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+
+public class 주식가격 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {1, 2, 3, 2, 3};
+        System.out.println(Arrays.toString(s.solution(arr)));
+    }
+}
